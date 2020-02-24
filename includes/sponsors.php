a52ffcfc6fe346fd575ba9b9a18e949cdd2f6acc
Bhaktij Koli <bhaktijkoli121@gmail.com>
2020-02-24 20:12:03 +0530

    Mobile version for sponsors

--- includes/sponsors.php
+++ includes/sponsors.php
@@ -10,8 +10,8 @@
         <div class="center">
           <h3>Associate Sponsor</h3>
         </div>
-        <div class="row">
-          <div class="col-sm-6 offset-3">
+        <div class="row justify-content-center">
+          <div class="col-sm-6">
             <div class="sponsor-logo-container" data-link="https://devfolio.co/">
               <img class="sponsor-logo" src="/dist/img/sponsors/devfolio.png"/>
             </div>
