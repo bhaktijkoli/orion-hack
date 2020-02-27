e63eb7c3a896a72168795aa06caa9666c91ab3eb
Bhaktij Koli <bhaktijkoli121@gmail.com>
2020-02-28 02:43:07 +0530

    Added footer

---
+++ includes/footer.php
@@ -0,0 +1,16 @@
+<div class="footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6">
+        <p class="copyright">©	2020 Orion Hack</p>
+      </div>
+      <div class="col-sm-6">
+        <div class="social-link-container">
+          <a href="https://www.instagram.com/orionhack2020/" class="social-link" target="_blank"><span class="typcn typcn-social-instagram"></span></a>
+          <a href="https://www.facebook.com/orionhack2020" class="social-link" target="_blank"><span class="typcn typcn-social-facebook"></span></a>
+          <a href="https://twitter.com/orion_hack" class="social-link" target="_blank"><span class="typcn typcn-social-twitter"></span></a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
--- index.php
+++ index.php
@@ -18,6 +18,7 @@
   <?php  include('includes/theme.php')?>
   <?php  include('includes/sponsors.php')?>
   <?php  include('includes/faq.php')?>
+  <?php  include('includes/footer.php')?>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/scrollReveal.js/4.0.5/scrollreveal.min.js"></script>
 <script type="text/javascript" src="/dist/js/particles.js"></script>
