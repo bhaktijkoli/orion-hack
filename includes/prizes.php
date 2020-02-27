61ff5dda42ecb10e6a63c518515686e34e3a8e50
Bhaktij Koli <bhaktijkoli121@gmail.com>
2020-02-28 03:38:18 +0530

    Added prizes

---
+++ includes/prizes.php
@@ -0,0 +1,32 @@
+<section id="prizes" class="prizes">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="section-title">
+          <h1>Prizes</h1>
+        </div>
+      </div>
+      <div class="col-sm-4 order-2 order-md-1">
+        <div class="prize">
+          <img class="silver" src="/dist/img/image-content/silver.png"/>
+          <p class="number">&#8377; 12,000</p>
+          <p class="label">Second Prize</p>
+        </div>
+      </div>
+      <div class="col-sm-4 order-1 order-md-2">
+        <div class="prize">
+          <img class="gold" src="/dist/img/image-content/gold.png"/>
+          <p class="number">&#8377; 15,000</p>
+          <p class="label">First Prize</p>
+        </div>
+      </div>
+      <div class="col-sm-4 order-3 order-md-3">
+        <div class="prize">
+          <img class="bronze" src="/dist/img/image-content/bronze.png"/>
+          <p class="number">&#8377; 9,000</p>
+          <p class="label">Thrid Prize</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
