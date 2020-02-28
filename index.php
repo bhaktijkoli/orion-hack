7441167fe68a62de727eceef417c78574371561d
Bhaktij Koli <bhaktijkoli121@gmail.com>
2020-02-28 14:58:13 +0530

    Updated meta tags, fixed mobile button

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Oriohhack website">
+  <meta name="theme-color" content="#EA4B33" />
+  <meta property=”og:title” content="Orion Hack"/>
+  <meta property=”og:image” content="/dist/img/logo-mini.svg"/>
+  <meta property=”og:site_name” content="orionhack.com"/>
+  <meta property=”og:description” content="Hackathon"/>
   <title>Orion Hack 2020</title>
   <link rel="stylesheet" type="text/css" href="/dist/css/app.css">
   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/typicons/2.0.9/typicons.min.css">
@@ -20,9 +26,9 @@
   <?php  include('includes/sponsors.php')?>
   <?php  include('includes/faq.php')?>
   <?php  include('includes/footer.php')?>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/scrollReveal.js/4.0.5/scrollreveal.min.js"></script>
-<script type="text/javascript" src="/dist/js/particles.js"></script>
-<script type="text/javascript" src="/dist/js/app.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/scrollReveal.js/4.0.5/scrollreveal.min.js"></script>
+  <script type="text/javascript" src="/dist/js/particles.js"></script>
+  <script type="text/javascript" src="/dist/js/app.js"></script>
 </body>
 </html>
