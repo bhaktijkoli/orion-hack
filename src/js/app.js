5a094a066a002721b164dd27bc9fe4139be66849
Bhaktij Koli <bhaktijkoli121@gmail.com>
2020-02-28 14:48:39 +0530

    Updated intro

--- src/js/app.js
+++ src/js/app.js
@@ -35,22 +35,32 @@ $("#navbar").hide();
     key: "orionhack"
   };
 
-  let script = document.createElement("script");
-  script.src = "https://apply.devfolio.co";
-  document.head.append(script);
-
-  script.onload = function() {
-    new Devfolio(devfolioOptions);
-  };
-
-  script.onerror = function() {
+  if( ( window.innerWidth <= 800 ) && ( window.innerHeight <= 600 ) ) {
     document
-      .querySelector(devfolioOptions.buttonSelector)
-      .addEventListener("click", function() {
-        window.location.href =
-          "https://devfolio.co/external-apply/" + devfolioOptions.key;
-      });
-  };
+    .querySelector(devfolioOptions.buttonSelector)
+    .addEventListener("click", function() {
+      window.location.href =
+      "https://devfolio.co/external-apply/" + devfolioOptions.key;
+    });
+  } else {    
+    let script = document.createElement("script");
+    script.src = "https://apply.devfolio.co";
+    document.head.append(script);
+
+    script.onload = function() {
+        new Devfolio(devfolioOptions);
+      };
+
+      script.onerror = function() {
+          document
+            .querySelector(devfolioOptions.buttonSelector)
+            .addEventListener("click", function() {
+                window.location.href =
+                  "https://devfolio.co/external-apply/" + devfolioOptions.key;
+              });
+          };
+  }
+
 })();
 
 function getRandomArbitrary(min, max) {
